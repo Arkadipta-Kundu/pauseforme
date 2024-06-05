2842ed54901c11f6dec76d7659530f6b3c2b4eb6
Arkadipta Kundu <arkadiptak49@gmail.com>
2024-06-05 13:21:33 +0530

    v1.1.0

--- pauseforme/content.js
+++ pauseforme/content.js
@@ -1,24 +1,48 @@
+let wasPausedByExtension = false;
+
 function pauseVideo() {
     const video = document.querySelector("video");
     if (video && !video.paused) {
+        wasPausedByExtension = true;
         video.pause();
     }
 }
 
 function resumeVideo() {
     const video = document.querySelector("video");
-    if (video && video.paused) {
+    if (video && video.paused && wasPausedByExtension) {
+        wasPausedByExtension = false;
         video.play();
     }
 }
 
-document.addEventListener("visibilitychange", () => {
+function handleVisibilityChange() {
     if (document.hidden) {
         pauseVideo();
     } else {
         resumeVideo();
     }
-});
+}
+
+function monitorManualPause() {
+    const video = document.querySelector("video");
+    if (video) {
+        video.addEventListener('pause', () => {
+            if (!wasPausedByExtension) {
+                wasPausedByExtension = false;
+            }
+        });
+
+        video.addEventListener('play', () => {
+            if (!document.hidden) {
+                wasPausedByExtension = false;
+            }
+        });
+    }
+}
+
+document.addEventListener("visibilitychange", handleVisibilityChange);
+monitorManualPause();
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "pauseVideo") {
